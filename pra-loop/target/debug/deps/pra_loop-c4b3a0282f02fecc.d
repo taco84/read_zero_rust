fa592338c4352aee6d7fea836297aeb39fd815b0
taco 84 <taco10ucd@gmail.com>
2023-08-28 05:41:34 +0000

    pra-loop

---
+++ pra-loop/target/debug/deps/pra_loop-c4b3a0282f02fecc.d
@@ -0,0 +1,5 @@
+/workspace/read_zero_rust/pra-loop/target/debug/deps/pra_loop-c4b3a0282f02fecc: src/main.rs
+
+/workspace/read_zero_rust/pra-loop/target/debug/deps/pra_loop-c4b3a0282f02fecc.d: src/main.rs
+
+src/main.rs:
---
+++ pra-loop/target/debug/pra-loop.d
@@ -0,0 +1,1 @@
+/workspace/read_zero_rust/pra-loop/target/debug/pra-loop: /workspace/read_zero_rust/pra-loop/src/main.rs
