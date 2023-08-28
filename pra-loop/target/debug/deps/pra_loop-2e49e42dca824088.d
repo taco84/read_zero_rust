9405fb55cc925cb3c17ea8a8b40748adef88784d
taco 84 <taco10ucd@gmail.com>
2023-08-28 06:01:47 +0000

    setting

---
+++ pra-loop/target/debug/deps/pra_loop-2e49e42dca824088.d
@@ -0,0 +1,5 @@
+/workspace/read_zero_rust/pra-loop/target/debug/deps/pra_loop-2e49e42dca824088.rmeta: src/main.rs
+
+/workspace/read_zero_rust/pra-loop/target/debug/deps/pra_loop-2e49e42dca824088.d: src/main.rs
+
+src/main.rs:
